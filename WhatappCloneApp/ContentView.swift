8206ef86331aaa8e77fc752e6ce3183933777898
bigmsg <travelcar@naver.com>
2019-11-21 06:26:34 +0900

    Initial Commit

---
+++ WhatappCloneApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatappCloneApp
+//
+//  Created by 관리자 on 2019/11/21.
+//  Copyright © 2019 관리자. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
